80d9fde5df87703bd05b282d201dd83f0841e4b2
Gevorg <kirakosyangev@yahoo.com>
2020-05-17 18:12:11 +0400

    add removeComponent and hrViewer functions in script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 var inputText = document.getElementById("todo"),
     list =document.getElementById("list"),
-    n = 2;
-console.log(list);
+    n = list.childElementCount;
+//console.log(list);
+function hrViewer() {
+    let hrTag = document.getElementsByTagName("hr");
+    console.log(hrTag);
+    if(n == 0){
+        hrTag[0].setAttribute("style", "display: none");
+    }
+    else
+        hrTag[0].setAttribute("style", "display: block");
+}
 function addComponent(event) {
     if (event.key == "Enter") {
         ++n;
@@ -20,15 +29,30 @@ function addComponent(event) {
         textTag.setAttribute("class", "text");
         textTag.innerText = txt1;
         let btnTag = document.createElement("button")
+
         btnTag.setAttribute("id","btn_"+n); 
+        btnTag.setAttribute("onclick","removeComponent(event)"); 
         component.appendChild(inputTag);
         component.appendChild(labelTag);
         component.appendChild(textTag);
         component.appendChild(btnTag);
         console.log(component);
         list.appendChild(component);
+        hrViewer();
     }
-}/*<div class="list-item">
+}
+function removeComponent(event) {
+
+    let btn = document.getElementById(event.target.id);
+    console.log(btn);
+    btn.parentElement.parentElement.removeChild(btn.parentElement);
+    n--;
+    hrViewer();
+
+}
+
+hrViewer();
+/*<div class="list-item">
     //<input type="checkbox" id="check1">
     //<label for="check1" class = "checkmark"></label>
     <div class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti nam sequi est, necessitatibus magni sit ex maxime minima eius, unde expedita. Fugit asperiores pariatur, illum fuga provident inventore maxime ducimus.</div>
