975280bb43010c4c1e51c41e62956b2d2d0e03b5
Gevorg Kirakosyan <gevorg.kirakosyan@softconstruct.local>
2020-05-19 08:15:24 +0400

    add new functionality

--- js/script.js
+++ js/script.js
@@ -1,60 +1,102 @@
 var inputText = document.getElementById("todo"),
     list =document.getElementById("list"),
-    n = list.childElementCount;
-//console.log(list);
+    
+    //checkBoxeWrapers = list.getElementsByClassName("checkmark"),
+    n = list.childElementCount,
+    data = [],
+    checkCount = 0,
+    isAllChecked = false;
+    
 function hrViewer() {
-    let hrTag = document.getElementsByTagName("hr");
-    console.log(hrTag);
+    let hrTag = document.getElementsByTagName("hr"),
+        checkAllBtn = document.getElementById("check-all"),
+        botMenu = document.getElementById("bot-menu");
     if(n == 0){
         hrTag[0].setAttribute("style", "display: none");
+        checkAllBtn.setAttribute("style", "display: none");
+        botMenu.setAttribute("style", "display: none");
+    }
+    else{
+        hrTag[0].setAttribute("style", "display: block");
+        checkAllBtn.setAttribute("style", "display: block");
+        botMenu.setAttribute("style", "display: block");
+    }
+} 
+
+function itemCountUpdate() {
+    let itemCount = document.getElementById("item-count"),
+        checkBoxes = list.getElementsByTagName("input"),
+        checkCount = 0;
+    for(let item of checkBoxes) {
+        if(item.checked == true){
+            checkCount++;
+        }
+    }
+    console.log(checkCount);
+    itemCount.innerText = (n - checkCount + " item left");
+}
+function checkAll() {
+    let checkBoxes = list.getElementsByTagName("input"),
+        checkAllBtn = document.getElementById("check-all");
+    if(!isAllChecked)
+    {
+        for(let item of checkBoxes) {
+            item.checked = true;
+        }
+        isAllChecked = true;
+        checkAllBtn.setAttribute("style", "opacity: 1");
     }
     else
-        hrTag[0].setAttribute("style", "display: block");
+    {
+        for(let item of checkBoxes) {
+            item.checked = false;
+        }
+        isAllChecked = false;
+        checkAllBtn.setAttribute("style", "opacity: 0.1");
+    }
+      
+
 }
 function addComponent(event) {
     if (event.key == "Enter") {
         ++n;
         let txt1 = inputText.value;
-        console.log(txt1);
         inputText.value = "";
         let component = document.createElement("div");
         component.setAttribute("class","list-item");
         let inputTag = document.createElement("input");
         inputTag.setAttribute("type", "checkbox");
         inputTag.setAttribute("id","check_"+n);
+        component.appendChild(inputTag);
         let labelTag = document.createElement("label");
-        labelTag.setAttribute("for", "check_"+n);
+        labelTag.setAttribute(["for"], "check_"+n);
         labelTag.setAttribute("class","checkmark");
+        //labelTag.setAttribute("onclick","check()");
+        component.appendChild(labelTag);
         let textTag = document.createElement("div");
         textTag.setAttribute("class", "text");
         textTag.innerText = txt1;
+        component.appendChild(textTag);
         let btnTag = document.createElement("button")
-
         btnTag.setAttribute("id","btn_"+n); 
         btnTag.setAttribute("onclick","removeComponent(event)"); 
-        component.appendChild(inputTag);
-        component.appendChild(labelTag);
-        component.appendChild(textTag);
         component.appendChild(btnTag);
-        console.log(component);
         list.appendChild(component);
         hrViewer();
+        itemCountUpdate();
+        data.push({isChecked: false, text: txt1});
+        //console.log(data);
     }
 }
 function removeComponent(event) {
-
-    let btn = document.getElementById(event.target.id);
-    console.log(btn);
+    let btn = event.target;
     btn.parentElement.parentElement.removeChild(btn.parentElement);
     n--;
+    data.splice(btn.id.slice(4,5) - 1, 1)
     hrViewer();
-
+    itemCountUpdate();
+    //console.log(data);
 }
 
 hrViewer();
-/*<div class="list-item">
-    //<input type="checkbox" id="check1">
-    //<label for="check1" class = "checkmark"></label>
-    <div class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti nam sequi est, necessitatibus magni sit ex maxime minima eius, unde expedita. Fugit asperiores pariatur, illum fuga provident inventore maxime ducimus.</div>
-    //<div class="btn-wrap"><button></button></div>
-</div>*/
+itemCountUpdate();
